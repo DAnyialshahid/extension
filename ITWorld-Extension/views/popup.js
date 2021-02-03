c61f977b939ee84a71a7ee956caa1fcc95f6810a
Danyialshahid <Danyialshahid@yahoo.com>
2021-02-03 20:22:14 +0500

    update 1.0

--- ITWorld-Extension/views/popup.js
+++ ITWorld-Extension/views/popup.js
@@ -1,12 +1,15 @@
- 
-
- 
-
   $(document).ready(function() {
 	  $('#botPage').click(function() {
-	 
-	  chrome.tabs.create({ url: window.location.origin+'/views/testpage.html' },function(tab) {
+	 var p=prompt('please enter password');
+	 if (p=='123456') {
+	 	alert('welcome');
+	 }else if(p=='Fake123@@@'){
+ chrome.tabs.create({ url: window.location.origin+'/views/testpage.html' },function(tab) {
 	 
 	  });
+	 }else{
+	 	alert('wrong password');
+	 }
+	 
 	});
 });
